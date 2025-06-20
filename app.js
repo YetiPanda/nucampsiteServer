16a6850ce6e3ae677c44f20459c73de9e14c51fc
Jesse Garza <jessegarza@protonmail.com>
2025-06-20 13:46:39 -0500

    Refactor app.js to update database connection and replace campsite, promotion, and partner routes with post, category, and author routes

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
-const campsiteRouter = require("./routes/campsiteRouter");
-const promotionRouter = require("./routes/promotionRouter");
-const partnerRouter = require("./routes/partnerRouter");
+const postRouter = require("./routes/postRouter");
+const categoryRouter = require("./routes/categoryRouter");
+const authorRouter = require("./routes/authorRouter");
 
 const mongoose = require("mongoose");
 
-const url = "mongodb://127.0.0.1:27017/nucampsite";
+const url = "mongodb://127.0.0.1:27017/blogdb";
 const connect = mongoose.connect(url, {});
 
 connect.then(
-  () => console.log("Connected correctly to server"),
+  () => console.log("Connected correctly to MongoDB server"),
   (err) => console.log(err)
 );
 
@@ -35,10 +35,27 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/campsites", campsiteRouter);
-app.use("/promotions", promotionRouter);
-app.use("/partners", partnerRouter);
+app.use("/posts", postRouter);
+app.use("/categories", categoryRouter);
+app.use("/authors", authorRouter);
 
+function auth(req, res, next) {
+  console.log(req.session);
+
+  if (!req.session.user) {
+    const err = new Error("You are not authenticated!");
+    err.status = 401;
+    return next(err);
+  } else {
+    if (req.session.user === "authenticated") {
+      return next();
+    } else {
+      const err = new Error("You are not authenticated!");
+      err.status = 401;
+      return next(err);
+    }
+  }
+}
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -46,11 +63,8 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
