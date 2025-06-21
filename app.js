59d103a776d2ae9ae21fb34d17a8031ad4c52d5a
Jesse Garza <jessegarza@protonmail.com>
2025-06-21 18:41:41 -0500

    Refactor code structure for improved readability and maintainability

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var cors = require("cors"); // Add CORS
+var cors = require("cors"); // Add this
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -24,11 +24,13 @@ connect.then(
 
 var app = express();
 
-// CORS configuration - Add this before other middleware
-app.use(cors({
-  origin: 'http://localhost:3001', // React dev server port
-  credentials: true
-}));
+// CORS configuration - Add this section
+app.use(
+  cors({
+    origin: "http://localhost:3001", // React dev server port
+    credentials: true,
+  })
+);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -38,38 +40,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// Temporarily comment out authentication for development
-// You can re-enable this later for admin routes only
-/*
-function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    const err = new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    return next(err);
-  }
-
-  const auth = Buffer.from(authHeader.split(" ")[1], "base64")
-    .toString()
-    .split(":");
-  const user = auth[0];
-  const pass = auth[1];
-  if (user === "admin" && pass === "password") {
-    return next(); // authorized
-  } else {
-    const err = new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    return next(err);
-  }
-}
-
-app.use(auth);
-*/
-
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
