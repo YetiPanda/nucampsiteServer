b3a3a6f6817ce6865a339f529a2fa9a571b6a4f4
Jesse Garza <jessegarza@protonmail.com>
2025-06-26 14:47:19 -0500

    Add HTTPS support and redirect HTTP requests to HTTPS; update .gitignore for server certificates

--- app.js
+++ app.js
@@ -35,6 +35,20 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(
+      `Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+    res.redirect(
+      301,
+      `https://${req.hostname}:${app.get("secPort")}${req.url}`
+    );
+  }
+});
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
